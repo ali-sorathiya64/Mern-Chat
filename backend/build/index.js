1dbc8ca2c3b0a5835e53c0b595d53033b5cf9cb6
ali-sorathiya64 <alisorathiya970@gmail.com>
2025-05-03 07:40:30 -0700

    add

--- backend/build/index.js
+++ backend/build/index.js
@@ -1,11 +1,10 @@
 import { createServer } from 'http';
-import passport from 'passport';
 import { Server } from 'socket.io';
 import { config } from './config/env.config.js';
-import { errorMiddleware } from './middlewares/error.middleware.js';
-import './passport/google.strategy.js';
 import { checkEnvVariables, env } from './schemas/env.schema.js';
+import { errorMiddleware } from './middlewares/error.middleware.js';
 import './config/cloudinary.config.js';
+import './passport/google.strategy.js';
 import attachmentRoutes from './routes/attachment.router.js';
 import authRoutes from './routes/auth.router.js';
 import chatRoutes from './routes/chat.router.js';
@@ -14,42 +13,37 @@ import requestRoutes from './routes/request.router.js';
 import userRoutes from './routes/user.router.js';
 import { socketAuthenticatorMiddleware } from './middlewares/socket-auth.middleware.js';
 import registerSocketHandlers from './socket/socket.js';
-const express = (await import("express")).default;
 checkEnvVariables();
+const express = (await import('express')).default;
+const cors = (await import('cors')).default;
+const cookieParser = (await import('cookie-parser')).default;
+const morgan = (await import('morgan')).default;
+const passport = (await import('passport')).default;
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        credentials: true,
-        origin: config.clientUrl,
-    },
+    cors: { credentials: true, origin: config.clientUrl },
 });
 app.set('io', io);
 export const userSocketIds = new Map();
-const bootstrap = async () => {
-    const { default: cors } = await import('cors');
-    const { default: cookieParser } = await import('cookie-parser');
-    const { default: morgan } = await import('morgan');
-    app.use(cors({ credentials: true, origin: config.clientUrl }));
-    app.use(passport.initialize());
-    app.use(express.json());
-    app.use(cookieParser());
-    app.use(morgan('tiny'));
-    app.use('/api/v1/auth', authRoutes);
-    app.use('/api/v1/chat', chatRoutes);
-    app.use('/api/v1/user', userRoutes);
-    app.use('/api/v1/request', requestRoutes);
-    app.use('/api/v1/message', messageRoutes);
-    app.use('/api/v1/attachment', attachmentRoutes);
-    io.use(socketAuthenticatorMiddleware);
-    app.get('/', (_, res) => {
-        res.status(200).json({ running: true });
-    });
-    app.use(errorMiddleware);
-    registerSocketHandlers(io);
-    server.listen(env.PORT, () => {
-        console.log(`Server [STARTED] ~ http://localhost:${env.PORT}`);
-        console.log(`Started in ${env.NODE_ENV} mode`);
-    });
-};
-bootstrap();
+app.use(cors({ credentials: true, origin: config.clientUrl }));
+app.use(passport.initialize());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('tiny'));
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/chat', chatRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/request', requestRoutes);
+app.use('/api/v1/message', messageRoutes);
+app.use('/api/v1/attachment', attachmentRoutes);
+io.use(socketAuthenticatorMiddleware);
+app.get('/', (_req, res) => {
+    res.status(200).json({ running: true });
+});
+app.use(errorMiddleware);
+registerSocketHandlers(io);
+server.listen(env.PORT, () => {
+    console.log(`server [STARTED] ~ http://localhost:${env.PORT}`);
+    console.log(`Started in ${env.NODE_ENV} mode`);
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,10 @@
-import type  { Request, Response } from 'express';
 import { createServer } from 'http';
-import passport from 'passport';
 import { Server } from 'socket.io';
 import { config } from './config/env.config.js';
-import { errorMiddleware } from './middlewares/error.middleware.js';
-import './passport/google.strategy.js';
 import { checkEnvVariables, env } from './schemas/env.schema.js';
+import { errorMiddleware } from './middlewares/error.middleware.js';
 import './config/cloudinary.config.js';
+import './passport/google.strategy.js';
 
 import attachmentRoutes from './routes/attachment.router.js';
 import authRoutes from './routes/auth.router.js';
@@ -17,53 +15,47 @@ import userRoutes from './routes/user.router.js';
 
 import { socketAuthenticatorMiddleware } from './middlewares/socket-auth.middleware.js';
 import registerSocketHandlers from './socket/socket.js';
-const express = (await  import ("express")).default;
 
 checkEnvVariables();
+
+const express = (await import('express')).default;
+const cors = (await import('cors')).default;
+const cookieParser = (await import('cookie-parser')).default;
+const morgan = (await import('morgan')).default;
+const passport = (await import('passport')).default;
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    credentials: true,
-    origin: config.clientUrl,
-  },
+  cors: { credentials: true, origin: config.clientUrl },
 });
 
 app.set('io', io);
 export const userSocketIds = new Map<string, string>();
 
-const bootstrap = async () => {
-  const { default: cors } = await import('cors');
-  const { default: cookieParser } = await import('cookie-parser');
-  const { default: morgan } = await import('morgan');
+app.use(cors({ credentials: true, origin: config.clientUrl }));
+app.use(passport.initialize());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('tiny'));
 
-  app.use(cors({ credentials: true, origin: config.clientUrl }));
-  app.use(passport.initialize());
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(morgan('tiny'));
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/chat', chatRoutes);
+app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/request', requestRoutes);
+app.use('/api/v1/message', messageRoutes);
+app.use('/api/v1/attachment', attachmentRoutes);
 
-  app.use('/api/v1/auth', authRoutes);
-  app.use('/api/v1/chat', chatRoutes);
-  app.use('/api/v1/user', userRoutes);
-  app.use('/api/v1/request', requestRoutes);
-  app.use('/api/v1/message', messageRoutes);
-  app.use('/api/v1/attachment', attachmentRoutes);
+io.use(socketAuthenticatorMiddleware);
 
-  io.use(socketAuthenticatorMiddleware);
+app.get('/', (_req, res) => {
+  res.status(200).json({ running: true });
+});
 
-  app.get('/', (_: Request, res: Response) => {
-    res.status(200).json({ running: true });
-  });
+app.use(errorMiddleware);
+registerSocketHandlers(io);
 
-  app.use(errorMiddleware);
-  registerSocketHandlers(io);
-
-  server.listen(env.PORT, () => {
-    console.log(`Server [STARTED] ~ http://localhost:${env.PORT}`);
-    console.log(`Started in ${env.NODE_ENV} mode`);
-  });
-};
-
-bootstrap();
+server.listen(env.PORT, () => {
+  console.log(`server [STARTED] ~ http://localhost:${env.PORT}`);
+  console.log(`Started in ${env.NODE_ENV} mode`);
+});
